e79aa1b97e59fdb4c02611c11857fe1c29d6bb4d
yuriy.katkov <viezz@yandex.ru>
2025-08-16 14:31:27 +0400

    Change localization

--- backend/users/models.py
+++ backend/users/models.py
@@ -11,6 +11,8 @@ class User(AbstractUser):
 
     class Meta:
         ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return self.email
