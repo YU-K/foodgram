1be53dc41b3b356fe88a6b22e11a97f8d4987b3b
yuriy.katkov <viezz@yandex.ru>
2025-09-04 19:13:44 +0400

    Add shopping_cart

--- backend/shopping_cart/admin.py
+++ backend/shopping_cart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ShoppingCart
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
--- backend/shopping_cart/models.py
+++ backend/shopping_cart/models.py
@@ -10,14 +10,20 @@ class ShoppingCart(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='cart_items',
+        verbose_name='Пользователь',
+        help_text='Пользователь, добавивший рецепт в корзину',
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
         related_name='in_carts',
+        verbose_name='Рецепт',
+        help_text='Рецепт, добавленный в корзину',
     )
 
     class Meta:
+        verbose_name = 'Элемент корзины'
+        verbose_name_plural = 'Список покупок'
         constraints = [
             models.UniqueConstraint(
                 fields=('user', 'recipe'),
