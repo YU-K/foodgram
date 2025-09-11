dc116dcec43db19883d048127045b9fff773ad77
yuriy.katkov <viezz@yandex.ru>
2025-09-11 20:14:51 +0400

    Migrate to Postgres

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/YU-K/foodgram" className={styles.textLink}>https://github.com/YU-K/foodgram</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/YU-K" className={styles.textLink}>Катков Юрий</a>
             </p>
           </div>
         </aside>
