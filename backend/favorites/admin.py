6a3dfd68d49e339cab086a8c8291a7f28c99d7e9
yuriy.katkov <viezz@yandex.ru>
2025-10-03 09:31:38 +0400

    Refactor favorites

--- backend/favorites/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Favorite
-
-
-@admin.register(Favorite)
-class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'recipe')
-    search_fields = ('user__username', 'recipe__name')
