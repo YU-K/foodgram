ae66be6cfee6780ba906198582fcdff38138c0ac
yuriy.katkov <viezz@yandex.ru>
2025-10-03 09:05:09 +0400

    Refactor shopping_cart

--- backend/shopping_cart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import ShoppingCart
-
-
-@admin.register(ShoppingCart)
-class ShoppingCartAdmin(admin.ModelAdmin):
-    list_display = ('user', 'recipe')
-    search_fields = ('user__username', 'recipe__name')
