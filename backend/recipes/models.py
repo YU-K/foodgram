f494f668ac13a8990ffe88767aae420752bdc80b
yuriy.katkov <viezz@yandex.ru>
2025-10-03 09:55:14 +0400

    Fix PEP8 warnings #2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import (
     MaxValueValidator,
