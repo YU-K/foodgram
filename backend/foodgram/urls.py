beee7cf77c3003fdc96e8f92fdb9237e3c4456ac
yuriy.katkov <viezz@yandex.ru>
2025-10-04 17:26:35 +0400

    Refactor urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,13 +2,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from recipes.views import recipe_short_link
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path('s/<int:recipe_id>/', recipe_short_link, name='recipe_short_link'),
+    path('', include('recipes.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'),
+         name='redoc'),
 ]
 
 if settings.DEBUG:
