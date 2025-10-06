bf325b0a808ff99662a4b5bf7ec95426fc8ba824
yuriy.katkov <viezz@yandex.ru>
2025-10-06 17:12:55 +0400

    Fix CSRF error

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,6 +12,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS',
+                                 '127.0.0.1, localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
