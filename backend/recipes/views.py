a40653f7ee33ef3e5d34e65a8357f63e558acd61
yuriy.katkov <viezz@yandex.ru>
2025-10-10 08:58:30 +0400

    Refactor recipes views

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import redirect
-from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Recipe
 
 
 def recipe_short_link(request, recipe_id):
-    if not Recipe.objects.filter(id=recipe_id).exists():
-        raise ValidationError(f'Рецепт с id={recipe_id} не найден')
+    get_object_or_404(Recipe, id=recipe_id)
     return redirect(f'/recipes/{recipe_id}/')
